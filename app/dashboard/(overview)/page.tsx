fdaaaad06bb6a4327b262c4a3fdd3da224be08b5
smd995 <112795934+smd995@users.noreply.github.com>
2024-12-26 11:54:36 +0900

    error 수정

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -11,12 +11,6 @@ import {
 } from "@/app/ui/skeletons";
 
 export default async function Page() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
